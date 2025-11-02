d2f0eb6617c6880fc843d5028bfeca4131d7eb79
Vaibhav <vaibhavchaubey786@gmail.com>
2025-11-02 18:19:31 +0530

    Initial Commit

---
+++ src/main/java/com/hms/media/MediaMicroserviceHmsApplication.java
@@ -0,0 +1,13 @@
+package com.hms.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaMicroserviceHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaMicroserviceHmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hms/media/MediaMicroserviceHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaMicroserviceHmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
